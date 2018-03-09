badc03fd19be2d7f0f2e07137d4a29e58ba0cef0
Tony Boyles <Anthony@Boyles.cc>
2018-03-09 12:24:51 -0500

    Added a boatload of stuff to the Appcache manifest
    Should be pretty close to capturing everything.

--- appcache.sh
+++ appcache.sh
@@ -5,14 +5,17 @@ echo "CACHE MANIFEST
 
 CACHE:
 index.html
-stylesheets/main.css
-scripts/app.js
-components/nav.html" > microbetrace.appcache
-
-cat index.html | grep -o "node_modules/.*.js" >> microbetrace.appcache
-cat index.html | grep -o "node_modules/.*.css" >> microbetrace.appcache
+stylesheets/main.css" > microbetrace.appcache
+ls components/ | sed -e 's/^/components\//' >> microbetrace.appcache
+ls workers/ | sed -e 's/^/workers\//' >> microbetrace.appcache
+ls vendor/ | sed -e 's/^/vendor\//' >> microbetrace.appcache
+ls img/ | sed -e 's/^/img\//' >> microbetrace.appcache
+cat index.html | grep -o "node_modules/.*\.js" >> microbetrace.appcache
+cat components/*.html | grep -o "node_modules/.*\.js" >> microbetrace.appcache
+cat index.html | grep -o "node_modules/.*\.css" >> microbetrace.appcache
+cat components/*.html | grep -o "node_modules/.*\.css" >> microbetrace.appcache
+ls data/ | sed -e 's/^/data\//' >> microbetrace.appcache
 
 echo "
 NETWORK:
-*
-" >> microbetrace.appcache
+*" >> microbetrace.appcache
