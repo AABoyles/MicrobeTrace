b46320c9daa6f06785e74cd658681e8542b84947
Tony Boyles <Anthony@Boyles.cc>
2018-03-14 11:09:01 -0400

    Work on the compute-mst webworker script. (Still not working, but it's a start.)

--- scripts/compute-mst.js
+++ scripts/compute-mst.js
@@ -1,22 +1,24 @@
-data.links.forEach(l => l.mst = false);
-data.links.sort((a, b) => a.distance - b.distance);
-data.links[0].mst = true;
-let nodes_in_tree = [data.links[0].source.id, data.links[0].target.id],
-    n = data.links.length;
-while(nodes_in_tree.length < data.nodes.length){
-  for(let i = 0; i < n; i++){
-    let link = data.links[i];
-    if(nodes_in_tree.includes(link.source.id) && !nodes_in_tree.includes(link.target.id)){
-      nodes_in_tree.push(link.target.id);
-      link.mst = true;
-      break;
-    }
-    if(nodes_in_tree.includes(link.target.id) && !nodes_in_tree.includes(link.source.id)){
-      nodes_in_tree.push(link.source.id);
-      link.mst = true;
-      break;
-    }
-  });
-}
-electron.ipcRenderer.send('update-data', data);
-electron.remote.getCurrentWindow().close();
+onmessage = function(e){
+  e.data.links.forEach(l => l.mst = false);
+  e.data.links.sort((a, b) => a.distance - b.distance);
+  e.data.links[0].mst = true;
+  var nodes_in_tree = [e.data.links[0].source.id, e.data.links[0].target.id],
+  n = e.data.links.length;
+  while(nodes_in_tree.length < e.data.nodes.length){
+    for(var i = 0; i < n; i++){
+      var link = e.data.links[i];
+      if(nodes_in_tree.includes(link.source.id) && !nodes_in_tree.includes(link.target.id)){
+        nodes_in_tree.push(link.target.id);
+        link.mst = true;
+        break;
+      }
+      if(nodes_in_tree.includes(link.target.id) && !nodes_in_tree.includes(link.source.id)){
+        nodes_in_tree.push(link.source.id);
+        link.mst = true;
+        break;
+      }
+    });
+  }
+  postMessage(e.data.links);
+  close();
+};
