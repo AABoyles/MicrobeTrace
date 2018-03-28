b861c888ffaecf2ddf35d505359ddb4593ee9c26
Tony Boyles <Anthony@Boyles.cc>
2018-03-28 13:12:24 -0400

    Fixed Server to run @Heroku

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 #!/usr/bin/env nodejs
 
-const { execFileSync } = require('child_process');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
-var port = process.env.PORT || 8080;
-execFileSync('node_modules/http-server/bin/http-server', ['-p', port]);
+const PORT = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext || '.html';
+  // maps file extention to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+    '.txt': 'text/plain',
+    '.gif': 'image/gif',
+    '.md': 'text/markdown'
+  };
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory search for index file matching the extention
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+}).listen(PORT);
+
+console.log(`Server listening on port ${PORT}`);
