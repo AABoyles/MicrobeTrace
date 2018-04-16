eddc4ccc710c2dd355963eda10961e93b2e1dba5
Tony Boyles <Anthony@Boyles.cc>
2018-04-16 16:57:54 -0400

    Added Post-hoc Distance Matrix computation
    So if somebody isn't headed to the heatmap or Phylogenetic tree, they won't need to wait around for it.

--- scripts/compute-dm.js
+++ scripts/compute-dm.js
@@ -1,37 +1,27 @@
-if(typeof window === 'undefined') window = self;
-importScripts('../node_modules/tn93/dist/tn93.min.js');
-
-function hamming(s1, s2, gapPenalty){
-  if(!gapPenalty) gapPenalty = 0;
-  var i = s1.length;
-  var sum = 0;
-  while(--i > 0){
-    if(s1[i] === '-' ^ s2[i] === '-'){
-      sum += gapPenalty;
-    } else if(s1[i] !== s2[i]){
-      sum++;
-    }
-  }
-  return sum;
-};
-
 onmessage = function(e){
-  var subset = e.data.nodes;
-  var j = e.data.j;
-  var n = subset.length;
-  var output = [];
+  var links = e.data.links;
+  var nodes = e.data.nodes;
+  var n = nodes.length;
+  var m = links.length;
+  var labels = nodes.map(s => s.id);
+  var tn93 = Array(n);
+  var snps = Array(n);
   for(var i = 0; i < n; i++){
-    if(i === j) continue;
-    var distance = tn93(subset[i]['seq'], subset[j]['seq'], 'AVERAGE');
-    var newLink = {
-      source: subset[i].id,
-      target: subset[j].id,
-      tn93: distance,
-      snps: hamming(subset[i]['seq'], subset[j]['seq'], 0),
-      distance: distance,
-      origin: ['Genetic Distance']
-    };
-    output.push(newLink);
+    tn93[i] = Array(n).fill(0);
+    snps[i] = Array(n).fill(0);
   }
-  postMessage(output);
+  for(var i = 0; i < m; i++){
+    var row = labels.findIndex(l => l === links[i].source);
+    var col = labels.findIndex(l => l === links[i].target);
+    if(row === -1 || col === -1) continue;
+    tn93[row][col] = links[i].tn93;
+    tn93[col][row] = links[i].tn93;
+    snps[row][col] = links[i].snps;
+    snps[col][row] = links[i].snps;
+  }
+  postMessage({
+    tn93: tn93,
+    snps: snps,
+    labels: labels
+  });
 };
