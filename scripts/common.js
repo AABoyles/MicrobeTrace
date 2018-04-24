cd92c9e3e417049d859c07bd63724b8f2603c7be
Tony Boyles <Anthony@Boyles.cc>
2018-04-24 16:43:05 -0400

    Removed Newick Parser
    Phylocanvas comes with one.

--- scripts/common.js
+++ scripts/common.js
@@ -130,20 +130,6 @@ app.parseFASTA = function(text){
   return seqs;
 };
 
-app.parseNewick = function(a){
-  for(var e = [], r = {}, s = a.split(/\s*(;|\(|\)|,|:)\s*/), t = 0; t < s.length; t++){
-    var n = s[t];
-    switch(n){
-      case '(': var c = {}; r.branchset = [c], e.push(r), r = c; break;
-      case ',': var c = {}; e[e.length-1].branchset.push(c), r = c; break;
-      case ')': r = e.pop(); break;
-      case ':': break;
-      default: var h = s[t-1]; ')' == h || '(' == h || ',' == h ? r.name = n : ':' == h && (r.length = parseFloat(n))
-    }
-  }
-  return r;
-};
-
 app.align = function(params){
   if(!params.cores) params.cores = 2;
   var n = params.nodes.length;
