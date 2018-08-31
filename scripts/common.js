526bfc3ad5a48550552d3fef1036f0141471ef8b
Tony Boyles <Anthony@Boyles.cc>
2018-08-31 14:29:02 -0400

    Default Color Links by Origin is disabled
    Temporarily, I promise! This is a workaround to make exposing a links-not getting rendered bug.

--- scripts/common.js
+++ scripts/common.js
@@ -27,7 +27,9 @@ app.sessionSkeleton = function(){
     messages: [],
     style: {
       nodeColors: [d3.schemeCategory10[0]].concat(d3.schemeCategory10.slice(2)),
-      nodeColorMap: function(){}
+      nodeColorMap: function(){},
+      linkColors: d3.schemePaired,
+      linkColorMap: function(){}
     }
   };
 };
