eb656a42a5e9ff62340454c4015ef090ac69267a
Tony Boyles <Anthony@Boyles.cc>
2018-09-20 17:02:39 -0400

    Added network background color to session

--- scripts/common.js
+++ scripts/common.js
@@ -54,6 +54,7 @@ app.sessionSkeleton = function(){
         'link-length': 0.125,
         'network-friction': 0.4,
         'network-gravity': 0.05,
+        'network-color': '#ffffff',
         'selected-color': '#ff8300',
         'selected-color-contrast': '#000000'
       }
