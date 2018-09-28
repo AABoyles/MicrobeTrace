273f192480b7a95d3cdd1109fce9a488b9fdc7b8
Tony Boyles <Anthony@Boyles.cc>
2018-09-28 08:45:14 -0400

    Update Appcache

--- appcache.sh
+++ appcache.sh
@@ -4,19 +4,29 @@ echo "CACHE MANIFEST
 # `date +%Y-%m-%d`
 
 CACHE:
-index.html
-stylesheets/main.css" > microbetrace.appcache
+index.html" > microbetrace.appcache
+
 ls components/ | sed -e 's/^/components\//' >> microbetrace.appcache
+
 ls scripts/ | sed -e 's/^/scripts\//' >> microbetrace.appcache
-ls vendor/ | sed -e 's/^/vendor\//' >> microbetrace.appcache
-ls img/ | sed -e 's/^/img\//' >> microbetrace.appcache
 cat index.html | grep -o "node_modules/.*\.js" >> microbetrace.appcache
 cat components/*.html | grep -o "node_modules/.*\.js" >> microbetrace.appcache
+cat scripts/*.js | grep -o "node_modules/.*\.js" >> microbetrace.appcache
+ls vendor/ | sed -e 's/^/vendor\//' >> microbetrace.appcache
+
+ls stylesheets/ | sed -e 's/^/stylesheets\//' >> microbetrace.appcache
 cat index.html | grep -o "node_modules/.*\.css" >> microbetrace.appcache
 cat components/*.html | grep -o "node_modules/.*\.css" >> microbetrace.appcache
+
+ls img/ | sed -e 's/^/img\//' >> microbetrace.appcache
+
 ls data/ | sed -e 's/^/data\//' >> microbetrace.appcache
 cat cache.extra >> microbetrace.appcache
 
 echo "
 NETWORK:
-*" >> microbetrace.appcache
+*
+
+SETTINGS:
+prefer-online
+" >> microbetrace.appcache
