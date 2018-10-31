b6abf89b83ad210314c8e9fbecafdc5c8a67db9f
Tony Boyles <aaboyles@Tonys-MacBook-Air.local>
2018-10-31 14:51:32 -0400

    Added a regex replacement of characters reserved by Newick Format to tree computer

--- scripts/compute-tree.js
+++ scripts/compute-tree.js
@@ -3,10 +3,13 @@ importScripts('../vendor/neighbor-joining.js')
 onmessage = function(e){
   var start = Date.now();
   var matrix = e.data.matrix;
-  var labels = e.data.labels.map(l => ({
-    'name': l,
-    'genotype': l
-  }));
+  var labels = e.data.labels.map(l => {
+    l = l.replace(/[;(),]*/g, '');
+    return {
+      'name': l,
+      'genotype': l
+    };
+  });
   var RNJ = new RapidNeighborJoining(matrix, labels);
   RNJ.run();
   console.log('Tree Compute time: ', ((Date.now()-start)/1000).toLocaleString(), 's');
