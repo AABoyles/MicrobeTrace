71618cb7421273098c488d26740763ccaa82bb4a
Tony Boyles <Anthony@Boyles.cc>
2018-12-11 16:47:59 -0500

    Attempt to redirect http to https

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 const express = require('express');
 
+const redirect = express.Router().get('/', function(req, res, next){
+  if(req.get('X-Forwarded-Proto') !== 'https'){
+    res.redirect('https://' + req.get('Host') + req.url);
+  }
+});
+
 express()
-  .use(express.static(__dirname))
+  .use(redirect, express.static(__dirname))
   .listen(process.env.PORT || 5000);
