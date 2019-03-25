24d0a113e5a75de06b0092ec664cf88778d33e30
Tony Boyles <Anthony@Boyles.cc>
2019-03-25 16:13:48 -0400

    Added a content security policy, both as a server header and a meta tag

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const compression = require('compression');
+const serveStatic = require('serve-static');
 
-var app = express().use(compression());
-
-app
-  .use(express.static(__dirname))
+express()
+  .use(serveStatic(__dirname, {
+    setHeaders: res => res.setHeader('Content-Security-Policy', "default-src 'self' 'unsafe-inline' 'unsafe-eval' data:; img-src * data:; script-src 'self' googletagmanager.com 'unsafe-inline' 'unsafe-eval'")
+  }))
+  .use(compression())
   .listen(process.env.PORT || 5000);
