e34f44e50f6a972f400fddf574a73ad082697240
Tony Boyles <Anthony@Boyles.cc>
2019-04-01 12:33:38 -0400

    Transitioned from CartoDB to Mapbox Light tiles

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const compression = require('compression');
 
 express()
   .use(express.static(__dirname, {
-    setHeaders: res => res.setHeader('Content-Security-Policy', "default-src 'self' 'unsafe-inline' 'unsafe-eval' data:; img-src * data:; script-src 'self' googletagmanager.com 'unsafe-inline' 'unsafe-eval'")
+    setHeaders: res => {
+      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' googletagmanager.com 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; img-src 'self' api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
+      //res.setHeader('HTTP-Strict-Transport-Security', 'max-age=1209600; includeSubDomains');
+    }
   }))
   .use(compression())
   .listen(process.env.PORT || 5000);
