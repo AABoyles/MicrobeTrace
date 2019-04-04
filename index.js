0a7e3a905c89e16dc80b10ee1386a45c29bbd3c7
Tony Boyles <Anthony@Boyles.cc>
2019-04-04 08:55:20 -0400

    Fixed content-security-policy for map tiles

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const compression = require('compression');
 express()
   .use(express.static(__dirname, {
     setHeaders: res => {
-      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' googletagmanager.com 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; img-src 'self' api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
       //res.setHeader('HTTP-Strict-Transport-Security', 'max-age=1209600; includeSubDomains');
+      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' googletagmanager.com; connect-src 'self' googletagmanager.com; img-src 'self' api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
     }
   }))
   .use(compression())
