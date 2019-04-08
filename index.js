977b7b9e00bbeee1e28daea10a3a45997481f248
Tony Boyles <Anthony@Boyles.cc>
2019-04-08 09:39:40 -0400

    Fixed content-security-policy for google analytics compatibility

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const compression = require('compression');
 express()
   .use(express.static(__dirname, {
     setHeaders: res => {
-      //res.setHeader('HTTP-Strict-Transport-Security', 'max-age=1209600; includeSubDomains');
-      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' googletagmanager.com; connect-src 'self' googletagmanager.com; img-src 'self' api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
+      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' googletagmanager.com *.google-analytics.com; connect-src 'self' googletagmanager.com; img-src 'self' *.google-analytics.com *.doubleclick.net www.google.com api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
+      res.setHeader('Strict-Transport-Security', 'max-age=1209600; includeSubDomains');
     }
   }))
   .use(compression())
