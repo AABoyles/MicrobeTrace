46fd264c89e40cd58f085d470c472dbeb890536e
Tony Boyles <Anthony@Boyles.cc>
2019-04-25 13:34:33 -0400

    Added doubleclick to CSP

--- serve.js
+++ serve.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 express()
   .use(express.static(__dirname, {
     setHeaders: res => {
-      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' googletagmanager.com *.google-analytics.com; connect-src 'self' googletagmanager.com *.google-analytics.com; img-src 'self' *.google-analytics.com *.doubleclick.net www.google.com api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
+      res.setHeader('Content-Security-Policy', "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' googletagmanager.com *.google-analytics.com; connect-src 'self' googletagmanager.com *.google-analytics.com stats.g.doubleclick.net; img-src 'self' *.google-analytics.com *.doubleclick.net www.google.com api.mapbox.com data:; style-src 'self' 'unsafe-inline'; manifest-src 'self'; font-src 'self';");
       res.setHeader('Strict-Transport-Security', 'max-age=1209600; includeSubDomains');
     }
   }))
