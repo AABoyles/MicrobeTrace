b986459460f4284fd42adba9d8c78c70da91d406
Tony Boyles <Anthony@Boyles.cc>
2019-05-09 16:18:40 -0400

    Updated excise script

--- excise.sh
+++ excise.sh
@@ -1,6 +1,10 @@
 rm -rf .git demo/ docs/ node_modules/ test/ \
-  *.md *.sh package-lock.json\
-  cache.extra Dockerfile postcss.config.js \
-  stylesheets/bundle.css
-echo "" > stylesheets/bundle.css
-echo "" > scripts/bundle.js
+      *.md *.sh package-lock.json\
+      cache.extra Dockerfile postcss.config.js \
+      stylesheets/bundle.css \
+      components/*.gz components/*.br \
+      data/*.gz data/*.br \
+      img/*.gz img/*.br \
+      scripts/*.gz scripts/*.br \
+      stylesheets/*.gz stylesheets/*.br \
+      workers/*.gz workers/*.br
