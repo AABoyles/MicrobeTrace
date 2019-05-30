3bab0b2806a7b510ece2a1ecb072050114293573
Tony Boyles <Anthony@Boyles.cc>
2019-05-30 12:29:18 -0400

    Fixed DMs to work with new distance scheme

--- workers/compute-dm.js
+++ workers/compute-dm.js
@@ -1,33 +1,37 @@
-onmessage = function(e){
+onmessage = function(e) {
   var start = Date.now();
-  var links = e.data.links, nodes = e.data.nodes, metrics = e.data.metrics;
-  var n = nodes.length, m = links.length, labels = nodes.map(s => s.id);
-  var output = {};
-  metrics.forEach(metric => output[metric] = Array(n));
-  for(var i = 0; i < n; i++){
-    metrics.forEach(metric => {
-      output[metric][i] = Array(n).fill(null);
-      output[metric][i][i] = 0;
-    });
+  var links = e.data.links,
+    nodes = e.data.nodes,
+    metric = e.data.metrics[0];
+  var n = nodes.length,
+    m = links.length,
+    labels = nodes.map(s => s.id);
+  var output = Array(n);
+  for (var i = 0; i < n; i++) {
+    output[i] = Array(n).fill(null);
+    output[i][i] = 0;
   }
-  for(var j = 0; j < m; j++){
+  for (var j = 0; j < m; j++) {
     var link = links[j];
     var row = labels.findIndex(l => l === link.source);
-    if(row === -1) continue;
+    if (row === -1) continue;
     var col = labels.findIndex(l => l === link.target);
-    if(col === -1) continue;
-    metrics.forEach(metric => {
-      if(metric in link){
-        output[metric][row][col] = link[metric];
-        output[metric][col][row] = link[metric];
-      }
-    });
+    if (col === -1) continue;
+    output[row][col] = link["distance"];
+    output[col][row] = link["distance"];
   }
-  output.labels = labels;
-  console.log('DM Compute time: ', (Date.now()-start).toLocaleString(), 'ms');
+  console.log("DM Compute time: ", (Date.now() - start).toLocaleString(), "ms");
   start = Date.now();
   var encoder = new TextEncoder();
   output = encoder.encode(JSON.stringify(output)).buffer;
-  postMessage({matrices: output, start: start}, [output]);
+  labels = encoder.encode(JSON.stringify(labels)).buffer;
+  postMessage(
+    {
+      matrix: output,
+      labels: labels,
+      start: start
+    },
+    [output, labels]
+  );
   close();
 };
