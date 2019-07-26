a67ec49653434d8900554264551cfa718ddf1049
Tony Boyles <Anthony@Boyles.cc>
2019-07-26 12:59:54 -0400

    Optimized Link Computation * Refined SNP Algorithm (~40% speedup on SNPs) * Streamlined distance computation, construction (~30% speedup all around) * Optimized data transfer (~100x speedup)

--- workers/compute-links.js
+++ workers/compute-links.js
@@ -1,14 +1,18 @@
-importScripts("../vendor/tn93.min.js");
+importScripts('../vendor/tn93.min.js');
 
 function snps(s1, s2) {
   //If we aligned them, these will definitely be the same length. If not...
   var n = Math.min(s1.length, s2.length);
   var sum = 0;
   for (var i = 0; i < n; i++) {
-    sum += (s1[i] !== s2[i]) & (s1[i] !== "-") & (s2[i] !== "-");
+    let c1 = s1[i];
+    let c2 = s2[i];
+    sum += (c1 != c2) & (c1 != '-') & (c2 != '-');
   }
   return sum;
 }
+
+var encoder = new TextEncoder();
 
 onmessage = function(e) {
   var start = Date.now();
@@ -17,32 +21,20 @@ onmessage = function(e) {
     n = subset.length,
     s = e.data.strategy.toUpperCase(),
     t = 0;
-  var output = Array((n * n - n) / 2);
-  for (var i = 0; i < n; i++) {
-    var source = subset[i];
-    for (var j = 0; j < i; j++) {
-      var target = subset[j];
-      var link = {
-        source: source.id,
-        target: target.id,
-        origin: ["Genetic Distance"]
-      };
-      if (metrics[0] == "snps") {
-        link.distance = snps(source["seq"], target["seq"]);
-      } else {
-        link.distance = tn93(source["seq"], target["seq"], s);
-      }
-      output[t++] = link;
+  var output = metrics[0] == 'snps' ?
+      new Uint16Array((n * n - n) / 2) :
+      new Float32Array((n * n - n) / 2);
+  for (let i = 0; i < n; i++) {
+    let source = subset[i];
+    for (let j = 0; j < i; j++) {
+      output[t++] = metrics[0] == 'snps' ?
+          snps(source['seq'], subset[j]['seq']) :
+          tn93(source['seq'], subset[j]['seq'], s);
     }
   }
-  console.log(
-    "Links Compute time: ",
-    (Date.now() - start).toLocaleString(),
-    "ms"
-  );
+  console.log('Links Compute time: ', (Date.now() - start).toLocaleString(), 'ms');
   start = Date.now();
-  var encoder = new TextEncoder();
-  output = encoder.encode(JSON.stringify(output)).buffer;
-  postMessage({ links: output, start: start }, [output]);
+  // output = encoder.encode(JSON.stringify(output)).buffer;
+  postMessage({ links: output.buffer, start: start }, [output.buffer]);
   close();
 };
