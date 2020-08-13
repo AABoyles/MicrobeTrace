50fa8b2b5416e4e52b020e0ff93b98e81e1be8ad
Tony Boyles <AABoyles@gmail.com>
2020-08-12 22:29:39 -0400

    Moved src/* to dist
    We didn't have a build step, so who cares?

--- scripts/excise.sh
+++ scripts/excise.sh
@@ -2,10 +2,6 @@
 
 scripts/cleanup.sh
 
-rm dist/common.js dist/index.js dist/main.css
-
-mv src/* dist
-
 rm -rf demo/ docs/ node_modules/ scripts/ src/ test/ \
       .* *.md *.sh package-lock.json \
       cache.extra Dockerfile sonar-project.properties\
---
+++ src/README.md
@@ -0,0 +1,3 @@
+# src
+
+This directory left for legacy purposes, and in case we move to a build system for which it's appropriate. Meanwhile, all the relevant files now live in dist/.
