128193df3dc57ee4ca3ee5c69efce2cdbbbfd85c
Tony Boyles <tony@ephemer.ai>
2020-08-18 21:20:24 -0400

    Update parse-fasta and parse-mega sws

--- workers/parse-fasta.js
+++ workers/parse-fasta.js
@@ -1,13 +1,14 @@
+const isblank = /^\s*$/;
+
 onmessage = function(e){
-  var start = Date.now();
-  var text = e.data;
+  let start = Date.now();
+  let text = e.data;
   if(!text || text.length == 0) return [];
-  var seqs = [], currentSeq = {};
-  var lines = text.split(/[\r\n]+/g);
-  var n = lines.length;
-  var isblank = /^\s*$/;
-  for(var i = 0; i < n; i++){
-    var line = lines[i].trim();  // issue 189, 190
+  let seqs = [], currentSeq = {};
+  let lines = text.split(/[\r\n]+/g);
+  let n = lines.length;
+  for(let i = 0; i < n; i++){
+    let line = lines[i].trim();  // issue 189, 190
     if(isblank.test(line) || line[0] == ';') continue;
     if(line[0] == '>'){
       if(i > 0) seqs.push(currentSeq);
@@ -22,8 +23,8 @@ onmessage = function(e){
   seqs.push(currentSeq);
   console.log('FASTA Parse time: ', (Date.now()-start).toLocaleString(), 'ms');
   start = Date.now();
-  var encoder = new TextEncoder();
-  var output = encoder.encode(JSON.stringify(seqs)).buffer;
+  let encoder = new TextEncoder();
+  let output = encoder.encode(JSON.stringify(seqs)).buffer;
   postMessage({nodes: output, start: start}, [output]);
   close();
 };
